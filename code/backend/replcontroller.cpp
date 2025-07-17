e0abbb06ccd7c40b5d39c0b93dd454d35d73002e
wedward <wehosea@gmail.com>
2025-07-17 14:19:54 -0500

    repl2 support

--- code/backend/replcontroller.cpp
+++ code/backend/replcontroller.cpp
@@ -1,5 +1,8 @@
 #include "replcontroller.h"
 #include <QDebug>
+// #include <QDir>
+#include <QCoreApplication>
+
 
 ReplController::ReplController(QObject *parent)
     : QObject(parent)
@@ -15,12 +18,21 @@ void ReplController::startRepl()
     replProcess = new QProcess(this);
 
     // Unbuffered output: add -u flag
-    QString program = "python.exe";
+    // QString program = "python.exe";
+    // QStringList arguments;
+    // arguments << "-u" << "repl2.py";
+
+    QString program = "repl2.exe";
     QStringList arguments;
-    arguments << "-u" << "repl2.py";
+    arguments << "";
+
+    // QString home = QDir::homePath();
 
     // Set working directory where python.exe and repl.py are
-    replProcess->setWorkingDirectory("C:/will/buildpy");
+    // replProcess->setWorkingDirectory(QDir::homePath());
+
+    QString appDir = QCoreApplication::applicationDirPath();
+    replProcess->setWorkingDirectory(appDir);
 
     connect(replProcess, &QProcess::readyReadStandardOutput,
             this, &ReplController::handleReadyRead);
@@ -31,6 +43,7 @@ void ReplController::startRepl()
     connect(replProcess, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
             this, &ReplController::handleFinished);
 
+    qDebug() << "[BACKEND]: Starting Build123d";
     replProcess->start(program, arguments);
 
     if (!replProcess->waitForStarted()) {
